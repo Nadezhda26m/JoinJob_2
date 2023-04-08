b11f976091538814144a8c8d18589a2bf305090f
Nadezhda26m <nadezhda-26m@mail.ru>
2023-04-08 18:02:13 +0300

    changed string format for Note

--- note_editor/core/models/Note.py
+++ note_editor/core/models/Note.py
@@ -1,16 +1,20 @@
 from datetime import datetime
 
+
 class Note:
-    def __init__(self, id: int, title, text, date_time: datetime):
-        self.id = id
+    def __init__(self, title, text, date_time=datetime.now()):
         self.title = title
         self.text = text
         self.date_time = date_time
 
-    def get_note_info(self):
-        return f'id_{self.id} title:{self.title}, date:{self.date_time.date()} ' \
-               f'{self.date_time.time().hour}:{self.date_time.time().minute}\ntext:{self.text}'
+    def __get_date(self):
+        return self.date_time.strftime("%d.%m.%Y %H:%M")
+
+    def __str__(self) -> str:
+        return f'<{self.title}> {self.__get_date()}\n\t{self.text}\n'
 
 
-new_note = Note(1, 'Note 1', 'Text 1', datetime.now())
-print(new_note.get_note_info())
+new_note = Note('Note 1', 'Text 1')
+print(new_note)
+new_note2 = Note('Note 2', 'Text 2')
+print(new_note2)
