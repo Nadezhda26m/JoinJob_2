b3f7ea04d45d4967f98d33bd53c4ca1c65ecc2b7
Nadezhda26m <nadezhda-26m@mail.ru>
2023-04-09 11:16:10 +0300

    changed init and repr in Note

--- note_editor/core/models/Note.py
+++ note_editor/core/models/Note.py
@@ -2,19 +2,24 @@ from datetime import datetime
 
 
 class Note:
-    def __init__(self, title, text, date_time=datetime.now()):
+    def __init__(self, title: str, text: str, date_time: datetime,
+                 note_id: int, short_text: int = 10):
         self.title = title
         self.text = text
         self.date_time = date_time
+        self.NOTE_ID = note_id
+        self.short_text = short_text
 
     def __get_date(self):
         return self.date_time.strftime("%d.%m.%Y %H:%M")
 
     def __str__(self) -> str:
-        return f'<{self.title}> {self.__get_date()}\n\t{self.text}\n'
+        return f'#{self.NOTE_ID:04} <{self.title}> {self.__get_date()}\n\t{self.text}'
+
+    def __repr__(self):
+        return f'#{self.NOTE_ID:04} <{self.title}> {self.__get_date()} ' \
+               f'>>> {self.text[:self.short_text]}...'
+
+    # def __eq__(self, other): pass
 
 
-new_note = Note('Note 1', 'Text 1')
-print(new_note)
-new_note2 = Note('Note 2', 'Text 2')
-print(new_note2)
