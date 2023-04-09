fd9da79cf9b96179c7e6fc79dd2d1a64a4310080
Nadezhda26m <nadezhda-26m@mail.ru>
2023-04-09 11:26:26 +0300

    changed some methods in Notepad and added example

--- note_editor/core/infrastructure/Notepad.py
+++ note_editor/core/infrastructure/Notepad.py
@@ -4,26 +4,57 @@ from JoinJob2NoteEditor.note_editor.core.models.Note import *
 class Notepad:
 
     def __init__(self):
-        self.list_notes = []
-        # self.index = index
+        self._list_notes = []
 
-    def _add_note(self, note: Note):
-        self.list_notes.append(note)  # добавит в конец списка
+    def add_note(self, note: Note):
+        self._list_notes.insert(0, note)
 
-    def _read_note(self, index: int):
-        if index < len(self.list_notes):
-            return self.list_notes[index]  # Note
+    def read_note(self, index: int):
+        if index < len(self._list_notes):
+            return self._list_notes[index]  # Note
         else:
             return False
 
-    def _read_all(self):
-        return self.list_notes
+    def read_all(self):
+        return self._list_notes
 
-    # def _change_note(self, index: int, parametr):
-    #     note = self.list_notes[index]
+    def change_note(self, index: int, parametr):  # параметр
+        note = self._list_notes.pop(index)
+        note.title = parametr  #
+        note.date_time = datetime.now()
+        self.add_note(note)
 
-    def _del_note(self, index: int):
-        self.list_notes.pop(index)
+    def del_note(self, index: int):
+        self._list_notes.pop(index)
 
-    def _del_all(self):
-        self.list_notes.clear()
+    def del_all(self):
+        self._list_notes.clear()
+
+    # def sort_notes(self):  #
+    #     pass
+
+
+my_notes = Notepad()
+
+a = 1
+while True:
+    if a % 3 == 0:
+        note_t = input("new title 2> ")
+        my_notes.change_note(1, note_t)
+    else:
+        note_t = input("title> ")
+        note_txt = input("text> ")
+        my_notes.add_note(Note(note_t, note_txt, datetime.now(), a))
+    a += 1
+
+    b = 1
+    for i in my_notes.read_all():
+        print(f'{b}. {i}')
+        b += 1
+    print()
+
+    note_list = my_notes.read_all()
+    size = len(note_list)
+    for i in range(size):
+        print(note_list[i].__repr__())
+    print()
