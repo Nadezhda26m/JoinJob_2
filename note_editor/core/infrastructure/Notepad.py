94022a6f4fe86541bc5d8cefe0496f973c62d22d
Nadezhda26m <nadezhda-26m@mail.ru>
2023-04-10 21:03:51 +0300

    add string format for Notepad

--- note_editor/core/infrastructure/Notepad.py
+++ note_editor/core/infrastructure/Notepad.py
@@ -6,6 +6,9 @@ class Notepad:
     def __init__(self):
         self._list_notes = []
 
+    def __str__(self):
+        return f"{self._list_notes}"
+
     def add_note(self, note: Note):
         self._list_notes.insert(0, note)
 
@@ -29,32 +32,3 @@ class Notepad:
 
     def del_all(self):
         self._list_notes.clear()
-
-    # def sort_notes(self):  #
-    #     pass
-
-
-my_notes = Notepad()
-
-a = 1
-while True:
-    if a % 3 == 0:
-        note_t = input("new title 2> ")
-        my_notes.change_note(1, note_t)
-    else:
-        note_t = input("title> ")
-        note_txt = input("text> ")
-        my_notes.add_note(Note(note_t, note_txt, datetime.now(), a))
-    a += 1
-
-    b = 1
-    for i in my_notes.read_all():
-        print(f'{b}. {i}')
-        b += 1
-    print()
-
-    note_list = my_notes.read_all()
-    size = len(note_list)
-    for i in range(size):
-        print(note_list[i].__repr__())
-    print()
