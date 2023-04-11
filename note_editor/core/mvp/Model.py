5d9893cf7da0dcf1e2e04c91751c11b8619aa2c6
Nadezhda26m <nadezhda-26m@mail.ru>
2023-04-11 19:50:17 +0300

    change init file

--- note_editor/core/mvp/Model.py
+++ note_editor/core/mvp/Model.py
@@ -1,15 +1,21 @@
 from abc import ABC, abstractmethod
+from JoinJob2NoteEditor.note_editor.core.dataformat.FormatNote import FormatNote
 from JoinJob2NoteEditor.note_editor.core.infrastructure.Notepad import Notepad
+from os.path import isfile
+from pathlib import Path
 
 
 class Model(ABC):
 
     @abstractmethod
-    def __init__(self, path, file_name, extension):
+    def __init__(self, path, file_name, extension, data_format: FormatNote):
         self.file = path + file_name + extension
+        self.data_format = data_format
+        if not isfile(self.file):
+            Path(self.file).touch()
 
     @abstractmethod
     def add_to_file(self, notepad: Notepad): pass
 
     @abstractmethod
-    def read_file(self): pass
+    def read_file(self) -> Notepad: pass
